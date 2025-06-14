a459a489e2ea8f5ce844129a36912a514b3a9ed8
hansolf <necreativnij@gmail.com>
2025-06-15 00:09:37 +0300

    доделан курс

--- course-microservice/pkg/routes/routes.go
+++ course-microservice/pkg/routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"course-microservice/pkg/course"
+	"github.com/gorilla/mux"
+)
+
+func RegisterCourseRoutes(r *mux.Router) {
+	r.HandleFunc("/courses", (&course.Course{}).Create).Methods("POST")
+	r.HandleFunc("/courses", course.GetAll).Methods("GET")
+	r.HandleFunc("/courses/{id}", (&course.Course{}).Update).Methods("PUT")
+	r.HandleFunc("/courses/{id}", course.Delete).Methods("DELETE")
+	r.HandleFunc("/courses/{id}/enroll", course.Enroll).Methods("POST")
+}
